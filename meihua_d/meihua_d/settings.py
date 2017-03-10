31375d192bc75f88e0d1135a6cd47442984045f7
root <root@localhost.localdomain>
2017-03-10 03:20:29 -0800

    Use mysql.
    Signed-off-by: root <root@localhost.localdomain>

--- meihua_d/meihua_d/settings.py
+++ meihua_d/meihua_d/settings.py
@@ -80,8 +80,10 @@ WSGI_APPLICATION = 'meihua_d.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'meuhua_db',
+        'USER': 'root',
+        'PASSWORD': '',
     }
 }
 
